cdf74296b887df9aaffdd5550662f1f9bf56e15a
Himanshu Garg <garg_himanshu@outlook.com>
2021-04-24 21:23:28 +0530

    Completion of Blood Use Case

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class Blood(BaseModel):
 
 
 class Oxygen(BaseModel):
-    mobileNumber: int = Query(...)
+    mobileNumber: str = Query(...)
     oxygenReciever: bool = Query(...)
     hospitalName: str = Query(...)
     fullGear: bool = Query(...)
@@ -36,6 +36,19 @@ class Oxygen(BaseModel):
     oxygenDetailsAvailable: bool = Query(...)
     latitude: float
     longitude: float
+
+class BloodReceive(BaseModel):
+    mobileNumber: str = Query(...)
+    bloodMessage: str = Query(...)
+    latitude: float = Query(...)
+    longitude: float = Query(...)
+
+class OxygenReceive(BaseModel):
+    mobileNumber: str = Query(...)
+    oxygenMessage: str = Query(...)
+    latitude: float = Query(...)
+    longitude: float = Query(...)
+
 
 app = FastAPI(
     title="COVAID Backend",
@@ -51,7 +64,7 @@ app = FastAPI(
 async def blood_entry(blood: Blood):
     with engine.connect() as conn:
         recoveryDate = blood.recoveryDate.split('T')[0] + " 00:00:00"
-        query = '''INSERT INTO bloodinfo(mobileNumber, bloodReceiver, bloodType, hospitalName, pickUpDrop, documentURI, 
+        query = '''INSERT INTO bloodInfo(mobileNumber, bloodReceiver, bloodType, hospitalName, pickUpDrop, documentURI, 
         recoveryDate, distanceWillingToTravel, detailsAvailable, latitude, longitude, createdAt, updatedAt) 
         values ('{0}',{1},{2},'{3}',{4},'{5}','{6}',{7},{8},{9},{10}, now(), now())'''.format(blood.mobileNumber,
                                                                                               blood.bloodReceiver,
@@ -68,7 +81,7 @@ async def blood_entry(blood: Blood):
 @app.post('/oxygen/entry')
 async def oxygen_entry(oxygen:Oxygen):
     with engine.connect() as conn:
-        query = '''INSERT INTO oxygeninfo(mobileNumber, oxygenReceiver, hospitalName, fullGear, canDeliver, 
+        query = '''INSERT INTO oxygenInfo(mobileNumber, oxygenReceiver, hospitalName, fullGear, canDeliver, 
                 detailsAvailable, latitude, longitude, createdAt, updatedAt) 
                 values ('{0}',{1},'{2}',{3},{4},{5},{6},{7},now(), now())'''.format(oxygen.mobileNumber, oxygen.oxygenReciever,
                                                                                     oxygen.hospitalName, oxygen.fullGear,
@@ -81,7 +94,7 @@ async def oxygen_entry(oxygen:Oxygen):
 @app.get('/blood/{mobileNumber}')
 async def blood_get(mobileNumber: str):
     with engine.connect() as conn:
-        query = '''SELECT * FROM bloodinfo WHERE mobileNumber = '{}' '''.format(mobileNumber)
+        query = '''SELECT * FROM bloodInfo WHERE mobileNumber = '{}' '''.format(mobileNumber)
         values = conn.execute(query)
 
         for value in values:
@@ -94,7 +107,7 @@ async def blood_get(mobileNumber: str):
 @app.get('/oxygen/{mobileNumber}')
 async def blood_get(mobileNumber: str):
     with engine.connect() as conn:
-        query = '''SELECT * FROM oxygeninfo WHERE mobileNumber = '{}' '''.format(mobileNumber)
+        query = '''SELECT * FROM oxygenInfo WHERE mobileNumber = '{}' '''.format(mobileNumber)
         values = conn.execute(query)
 
         for value in values:
@@ -104,4 +117,107 @@ async def blood_get(mobileNumber: str):
             'status': 400
         }
 
+@app.post('/blood/receive/')
+async def blood_receive_request(bloodreceive: BloodReceive):
+    with engine.connect() as conn:
+        query =''' SELECT mobileNumber, ( 6371 * acos( cos( radians({0}) ) * cos( radians( latitude ) )
+                * cos( radians( longitude ) - radians({1}) ) + sin( radians({2}) ) * sin(radians(latitude)) ) ) AS distance
+                FROM bloodInfo 
+                WHERE bloodReceiver = 0 AND isActive = 1
+                HAVING distance < 500
+                ORDER BY distance
+                LIMIT 0 , 3;'''.format(bloodreceive.latitude, bloodreceive.longitude, bloodreceive.latitude)
 
+        values = conn.execute(query)
+        k = 0
+        for value in values:
+            query2 = ''' INSERT INTO bloodMapping(receiver, donor, distance) VALUES ('{}','{}',{})
+            '''.format(bloodreceive.mobileNumber, value[0], value[1])
+            res2 = conn.execute(query2)
+            k+=1
+
+        query3 = '''INSERT INTO customMessage(mobileNumber, message, contentType) 
+        VALUES('{0}','{1}',0)
+        '''.format(bloodreceive.mobileNumber, bloodreceive.bloodMessage)
+        res3 = conn.execute(query3)
+
+        return {
+            "userNotified": k
+        }
+
+@app.get('/blood/donate/{mobileNumber}')
+async def blood_donate_data(mobileNumber: str):
+    with engine.connect() as conn:
+        query0 = ''' SELECT bI.bloodType, bI.hospitalName, bI.pickUpDrop, bI.documentURI, bM.donor, bM.receiver 
+        FROM bloodInfo as bI, bloodMapping as bM 
+        WHERE bM.donor = '{}' AND bM.isAccepted = 1 and bI.mobileNumber = bM.receiver
+        '''.format(mobileNumber)
+        values = conn.execute(query0)
+        for value in values:
+            return  [value]
+
+        query = '''SELECT bI.bloodType, bI.hospitalName, bI.pickUpDrop, bI.documentURI, cM.message, bM.distance
+                    FROM bloodInfo as bI
+                    RIGHT JOIN bloodMapping as bM on bM.receiver = bI.mobileNumber
+                    LEFT JOIN customMessage cM on bI.mobileNumber = cM.mobileNumber
+                    WHERE bM.donor = '{}' and bI.isActive = 1 and cM.contentType = 0
+        '''.format(mobileNumber)
+
+        values = conn.execute(query)
+        respones = []
+        for value in values:
+            respones.append(value)
+
+        return respones
+
+
+#Needs to be tested briefly
+@app.post('/blood/accept/{donor}/{receiver}')
+def blood_accept(donor: str, receiver: str):
+    with engine.connect() as conn:
+        query0 = ''' UPDATE bloodMapping SET isAccepted = 1 where donor = '{0}' and receiver = '{1}'
+        '''.format(donor, receiver)
+
+        conn.execute(query0)
+
+        query1 = '''UPDATE bloodInfo SET isActive = 0 where mobileNumber = '{0}' or mobileNumber = '{1}'
+        '''.format(donor, receiver)
+
+        conn.execute(query1)
+
+        query2 = '''DELETE FROM bloodMapping where (donor = '{0}' or receiver = '{0}') and isAccepted = 0
+        '''.format(donor, receiver)
+
+        conn.execute(query2)
+
+        return {
+            "message": "Request Processed Successfully"
+        }
+
+@app.post('/oxygen/receive/')
+async def oxygen_receive_request(oxygenreceive: OxygenReceive):
+    with engine.connect() as conn:
+        query =''' SELECT mobileNumber, ( 6371 * acos( cos( radians({0}) ) * cos( radians( latitude ) )
+                * cos( radians( longitude ) - radians({1}) ) + sin( radians({2}) ) * sin(radians(latitude)) ) ) AS distance
+                FROM oxygenInfo 
+                WHERE oxygenReceiver = 0 AND isActive = 1
+                HAVING distance < 500
+                ORDER BY distance
+                LIMIT 0 , 3;'''.format(oxygenreceive.latitude, oxygenreceive.longitude, oxygenreceive.latitude)
+
+        values = conn.execute(query)
+        k = 0
+        for value in values:
+            query2 = ''' INSERT INTO oxygenMapping(receiver, donor, distance) VALUES ('{}','{}',{})
+            '''.format(oxygenreceive.mobileNumber, value[0], value[1])
+            res2 = conn.execute(query2)
+            k+=1
+
+        query3 = '''INSERT INTO customMessage(mobileNumber, message, contentType) 
+        VALUES('{0}','{1}',1)
+        '''.format(oxygenreceive.mobileNumber, oxygenreceive.oxygenMessage)
+        res3 = conn.execute(query3)
+
+        return {
+            "userNotified": k
+        }
