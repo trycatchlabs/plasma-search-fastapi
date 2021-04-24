15cbde97206ac4c7a0563ea081bee714d3a8625d
Himanshu Garg <garg_himanshu@outlook.com>
2021-04-24 18:07:13 +0530

    Add Endpoint for basic op on blood and O2

--- main.py
+++ main.py
@@ -1,15 +1,107 @@
-from typing import Optional
-
-from fastapi import FastAPI
+import os
+from pydantic import BaseModel
+from fastapi import FastAPI, Query
+from sqlalchemy import create_engine
 
 app = FastAPI()
 
+host = os.environ['HOST']
+user = os.environ['USER']
+password = os.environ['PASSWORD']
+database = os.environ['DATABASE']
+port = os.environ['PORT']
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+engine = create_engine('mysql+pymysql://{}:{}@{}/{}'.format(user,password,host,database))
+
+class Blood(BaseModel):
+    mobileNumber: str
+    bloodReceiver: bool
+    bloodType: int
+    hospitalName: str
+    pickUpDrop: bool
+    documentURI: str
+    recoveryDate: str
+    distanceWillingToTravel: int
+    detailsAvailable: bool
+    latitude: float
+    longitude: float
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+class Oxygen(BaseModel):
+    mobileNumber: int = Query(...)
+    oxygenReciever: bool = Query(...)
+    hospitalName: str = Query(...)
+    fullGear: bool = Query(...)
+    canDeliver: bool = Query(...)
+    oxygenDetailsAvailable: bool = Query(...)
+    latitude: float
+    longitude: float
+
+app = FastAPI(
+    title="COVAID Backend",
+    description="Backend for COVAID",
+    version="0.1.0",
+    openapi_url="/api/v0.1.0/openapi.json",
+    docs_url="/",
+    redoc_url=None
+)
+
+
+@app.post('/blood/entry')
+async def blood_entry(blood: Blood):
+    with engine.connect() as conn:
+        recoveryDate = blood.recoveryDate.split('T')[0] + " 00:00:00"
+        query = '''INSERT INTO bloodinfo(mobileNumber, bloodReceiver, bloodType, hospitalName, pickUpDrop, documentURI, 
+        recoveryDate, distanceWillingToTravel, detailsAvailable, latitude, longitude, createdAt, updatedAt) 
+        values ('{0}',{1},{2},'{3}',{4},'{5}','{6}',{7},{8},{9},{10}, now(), now())'''.format(blood.mobileNumber,
+                                                                                              blood.bloodReceiver,
+                                                                                   blood.bloodType, blood.hospitalName,
+                                                                                   blood.pickUpDrop, blood.documentURI,
+                                                                                   recoveryDate, blood.distanceWillingToTravel,
+                                                                                   blood.detailsAvailable, blood.latitude,
+                                                                                   blood.longitude)
+
+        status = conn.execute(query)
+        return "Hello"
+
+
+@app.post('/oxygen/entry')
+async def oxygen_entry(oxygen:Oxygen):
+    with engine.connect() as conn:
+        query = '''INSERT INTO oxygeninfo(mobileNumber, oxygenReceiver, hospitalName, fullGear, canDeliver, 
+                detailsAvailable, latitude, longitude, createdAt, updatedAt) 
+                values ('{0}',{1},'{2}',{3},{4},{5},{6},{7},now(), now())'''.format(oxygen.mobileNumber, oxygen.oxygenReciever,
+                                                                                    oxygen.hospitalName, oxygen.fullGear,
+                                                                                    oxygen.canDeliver, oxygen.oxygenDetailsAvailable,
+                                                                                    oxygen.latitude, oxygen.longitude)
+
+        status = conn.execute(query)
+        return "Hello"
+
+@app.get('/blood/{mobileNumber}')
+async def blood_get(mobileNumber: str):
+    with engine.connect() as conn:
+        query = '''SELECT * FROM bloodinfo WHERE mobileNumber = '{}' '''.format(mobileNumber)
+        values = conn.execute(query)
+
+        for value in values:
+            return value
+
+        return {
+            'status': 400
+        }
+
+@app.get('/oxygen/{mobileNumber}')
+async def blood_get(mobileNumber: str):
+    with engine.connect() as conn:
+        query = '''SELECT * FROM oxygeninfo WHERE mobileNumber = '{}' '''.format(mobileNumber)
+        values = conn.execute(query)
+
+        for value in values:
+            return value
+
+        return {
+            'status': 400
+        }
+
+
